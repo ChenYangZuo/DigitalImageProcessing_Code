558a662d6214215c380924a970794b72d117b42b
ChenYangZuo <zuo-cy2008@163.com>
2020-02-19 17:21:31 +0800

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# DigitalImageProcessing_Code
---
+++ cmake-build-debug/CMakeFiles/DigitalImageProcessing_Code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DigitalImageProcessing_Code.dir/main.cpp.o"
+  "DigitalImageProcessing_Code"
+  "DigitalImageProcessing_Code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DigitalImageProcessing_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
