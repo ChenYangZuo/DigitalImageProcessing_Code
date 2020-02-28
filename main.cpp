b1fffa19405ed82bf27e6245256f1c752caa3c2c
ChenYangZuo <zuo-cy2008@163.com>
2020-02-28 14:50:19 +0800

    updata

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace cv;
+using namespace std;
+
+
 int main()
 {
-    Mat img = imread("/home/zuochenyang/Desktop/DigitalImageProcessing_Code/test.jpg");
+    cout << "Hello,World!";
+    Mat img = imread("/home/zuochenyang/Desktop/DigitalImageProcessing_Code/test.jpg",0);
+
+
+
+
     imshow("test", img);
     waitKey(0);
     return 0;
 }
+
