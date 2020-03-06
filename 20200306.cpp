2029062218acc5230c209c480442052838fd0b8f
ChenYangZuo <zuo-cy2008@163.com>
2020-03-06 14:30:42 +0800

    fix some bugs

--- 20200306.cpp
+++ 20200306.cpp
@@ -42,11 +42,22 @@ int exercise_2(){
 }
 
 int exercise_3(){
-    Point pt;
+    Point pt,pt1,pt2;
     Mat img = imread("/home/zuochenyang/Desktop/DigitalImageProcessing_Code/test1.png");
     pt.x = 100;
     pt.y = 200;
+    pt1.x = 360;
+    pt1.y = 360;
+    pt2.x = 240;
+    pt2.y = 240;
+    Rect rect;
+    rect.x = 360;
+    rect.y = 240;
+    rect.width = 120;
+    rect.height = 240;
     circle(img,pt,80,CV_RGB(0,255,0),5,8,0);
+    line(img,pt1,pt2,CV_RGB(0,255,0),3,8,0);
+    rectangle(img,rect,CV_RGB(0,255,0),3,8,0);
     imshow("exercise_3",img);
     waitKey(0);
     return 0;
@@ -59,8 +70,8 @@ int exercise_4(){
 int main(){
     cout << "Hello,World!";
 //    exercise_1();
-//    exercise_3();
-    exercise_2();
+    exercise_3();
+//    exercise_2();
 
     return 0;
 }
