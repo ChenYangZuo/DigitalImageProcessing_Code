f8a06e6dc21d82486b88f511393d7b732b1eb011
ChenYangZuo <zuo-cy2008@163.com>
2020-03-20 13:29:23 +0800

    add 20200320

--- 20200313.cpp
+++ 20200313.cpp
@@ -46,6 +46,7 @@ int exercise_1(){
         cvtColor(frame,hsvMat,COLOR_BGR2HSV);
         frame.copyTo(detectMat);
 
+        
         inRange(hsvMat,Scalar(i_minH,i_minS,i_minV),Scalar(i_maxH,i_maxS,i_maxV),detectMat);
 
         imshow("2",detectMat);
---
+++ 20200320.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+    cout << "Hello,World!";
+    return 0;
+}
